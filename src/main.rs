1134907255f505adb40e1a187eee9e2d4e645eb1
Fernando Dinar Setiawan <fernandosetiawan757@gmail.com>
2026-06-29 22:06:35 +0700

    feat: asynchronous L4/L7 gateway core powered by tokio

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,37 @@
-use std::{
-    io::{BufReader, prelude::*},
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::{TcpListener, TcpStream},
-    thread,
-    time::Duration, // Untuk simulasi tes beku nanti
+    time::{Duration, sleep},
 };
 
-fn main() {
-    let listener = match TcpListener::bind("127.0.0.1:8080") {
+#[tokio::main]
+async fn main() {
+    let listener = match TcpListener::bind("127.0.0.1:8080").await {
         Ok(tl) => {
-            println!("[RustGate Frontline] System online. Listening on port 8080...");
+            println!("[RustGate Frontline] Async Gateway v0.1.0 online. Listening on 8080...");
             tl
         }
         Err(e) => panic!("error: {e}"),
     };
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        thread::spawn(move || {
-            handle_connection(stream);
+    loop {
+        let (stream, _socket_addr) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            handle_connection(stream).await;
         });
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    thread::sleep(Duration::from_secs(5));
-    let buf_reader = BufReader::new(&stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+async fn handle_connection(mut stream: TcpStream) {
+    sleep(Duration::from_secs(5)).await;
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next_line().await.unwrap().unwrap();
 
-    println!("[Link Established] Target URL: {request_line}");
+    println!("[Async Link] Target URL: {request_line}");
     let status_line = "HTTP/1.1 200 OK";
-    let content = "<h1>RustGate v0.0.1 Operational</h1>";
+    let content = "<h1>RustGate v0.1.0 Async Operational</h1>";
     let length = content.len();
 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{content}");
-    stream.write_all(response.as_bytes()).unwrap();
+    stream.write_all(response.as_bytes()).await.unwrap();
 }
